1174cfb86faac668c8aaa52bd8ceb149b48cf26e
alsdn733 <33590839+alsdn733@users.noreply.github.com>
2018-06-03 18:09:51 +0900

    implement main page with parallax scrolling
    * Make main page layout with parallex
    
    * implement main page with parallax scrolling
     
    PR-URL: https://github.com/Lyceum519/SoloEscape_Web_Client/pull/22#pullrequestreview-125388717

--- src/views/main/Main.jsx
+++ src/views/main/Main.jsx
@@ -1,5 +1,18 @@
 import React, { Component } from 'react'
 import * as SoloAPI from '../../apis/SoloEscape.js'
+import { Parallax } from 'react-parallax';
+
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+
+const insideStyles = {background: 'white', padding: 20, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'};
+const image1 = require('./coupleimg1.jpg');
+const image2 = require('./coupleimg2.jpg');
+const image3 = require('./coupleimg3.jpg');
 
 class Main extends Component {
 
@@ -8,11 +21,36 @@ class Main extends Component {
 
   render() {
     return (
-      <div>Hello world!
-        {/* <img src={require("../../images/solo.png")} /> */}
+      
+    <div style={styles}>
+    <h1>Hello SoloEscape</h1>
+    <Parallax bgImage={image1}
+      strength={500}>
+      <div style={{height: 500}}>
+        <div style={insideStyles}>#그동안 없었던</div>
       </div>
+    </Parallax>
+    <div style={{lineHeight: 8}}><br/></div>
+    <Parallax bgImage={image3} 
+      strength={500}>
+      <div style={{height: 500}}>
+        <div style={insideStyles}>#특별한 소개팅의 시작</div>
+      </div>
+    </Parallax>
+    <div style={{lineHeight: 8}}><br/></div>
+    <Parallax bgImage={image2} 
+      strength={500}>
+      <div style={{height: 500}}>
+        <div style={insideStyles}>#솔로 이스케이프</div>
+      </div>
+    </Parallax>
+    <div style={{height: 0}}>
+      </div>
+    <h2>{'\u2728'}</h2>
+  </div>
     )
   }
 }
+
 
 export default Main;
